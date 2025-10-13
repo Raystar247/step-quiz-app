0a8ac027ac960ac422b2e0d733f2c20375462d92
Raystar247 <24star10@gmail.com>
2025-10-14 08:18:29 +0900

    Add ScoringSheet on sketch

--- client/src/features/stepq/components/ChatView.tsx
+++ client/src/features/stepq/components/ChatView.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import UserQuestionToggle from './UserQuestionToggle';
+import ScoringSheet from './ScoringSheet';
 
 const ChatView: React.FC = () => {
   const [view, setView] = useState<'user' | 'question'>('user');
@@ -28,6 +29,9 @@ const ChatView: React.FC = () => {
             <line x1="16.5" y1="16.5" x2="21" y2="21" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
           </svg>
         </button>
+      </div>
+      <div className='p-10'>
+        <ScoringSheet />
       </div>
     </div>
   );
