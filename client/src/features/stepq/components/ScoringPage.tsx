9d0cbb2cbd3a9c0a4a8b12f0cb2c15d81333255f
Raystar247 <24star10@gmail.com>
2025-10-19 23:00:46 +0900

    採点用のデータセットを追加
    採点用のデータセットとしてAnswer型について、正答と問題のindexを含めたものを型として作り、データを整形する処理を追加した

--- client/src/features/stepq/components/ScoringPage.tsx
+++ client/src/features/stepq/components/ScoringPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ENDPOINT_URL } from "../../../references/util";
 import axios from "axios";
@@ -6,12 +6,48 @@ import type { Answer, Question, Trial, UnitString } from "../type";
 import stepqApi from "../api/stepqApi";
 import SelectHeader from "./SelectHeader";
 
+type ScoringFormattedAnswer = {
+    index: number;
+    correctAnswer: string;
+    answer: Answer;
+};
 
 const ScoringPage = () => {
     const urlParam = useParams<{ qgroupId: string }>();
     const [unit, setUnit] = useState<UnitString>('user');
     const [answers, setAnswers] = useState<Answer[]>([]);
+    const [formattedAnswers, setFormattedAnswers] = useState<ScoringFormattedAnswer[]>([]);
 
+    // 解答データに問題番号・正解を付与して配列として返す関数
+    const formatAnswerForScoring = async (answers: Answer[]): Promise<ScoringFormattedAnswer[]> => {
+        // 解答データに問題番号・正解を付与する内部関数
+        const addInfo = (ans: Answer, questions: Question[]): ScoringFormattedAnswer => {
+            const question: Question | undefined = questions.find(_q => _q.id == ans.questionId);
+            if (!question) { return { index: -1, correctAnswer: '', answer: ans }; }  // エラー回避用ダミー
+            const formattedData: ScoringFormattedAnswer = {
+                index: question.index,
+                correctAnswer: question.correctAnswer,
+                answer: ans
+            };
+            return formattedData;
+        }
+
+        if (!urlParam.qgroupId) { return []; }
+        const allQuestions = await stepqApi.fetchQuestionsOfQGroup(urlParam.qgroupId);
+        if (!allQuestions) { return []; }
+        const formattedAnswers = answers.map(ans => addInfo(ans, allQuestions));
+        return formattedAnswers;
+    };
+
+    useEffect(() => {
+        const f = async () => {
+            const formatted = await formatAnswerForScoring(answers);
+            setFormattedAnswers(formatted);
+            console.log("formatted answers:");
+            console.log(formatted);
+        };
+        f();
+    }, [answers]);
 
 
     return (<SelectHeader
